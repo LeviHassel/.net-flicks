f39ce573eff4d162aa0fd747d986f59bfb9dea13
Levi Hassel <levihassel@gmail.com>
2018-02-16 22:41:30 -0600

    Got bootstrap-select working

--- CoreTemplate/CoreTemplate.Web/wwwroot/js/site.js
+++ CoreTemplate/CoreTemplate.Web/wwwroot/js/site.js
@@ -1,10 +1,4 @@
 ﻿$(function () {
-    //Set up Tempus Dominus DateTime Picker: https://tempusdominus.github.io/bootstrap-4/
-    $("#datetimepicker").datetimepicker({
-        format: "L",
-        viewMode: "years"
-    });
-
     //Add New Person in Edit Crew modal
     $(document).on("click", ".add-person", function () {
         var nextIndex = $("#people-table tbody tr").length;
@@ -15,6 +9,9 @@
             type: "GET"
         }).done(function (response) {
             $("#people-table tbody").append(response);
+
+            //Refresh bootstrap-select so that it sets up the JS for new dropdowns
+            $(".selectpicker").selectpicker();
         });
     });
 
@@ -22,6 +19,5 @@
     $("#people-table").on("click", ".delete-person", function () {
         $(this).closest("td").find("input").val("true");
         $(this).closest("tr").hide();
-        
     });
 });

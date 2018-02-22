c8e00d98d72333872fe7cca2258ceb3e82b7762e
Levi Hassel <levihassel@gmail.com>
2018-02-21 22:50:57 -0600

    Added image modals

--- CoreTemplate/CoreTemplate.Web/wwwroot/js/site.js
+++ CoreTemplate/CoreTemplate.Web/wwwroot/js/site.js
@@ -3,24 +3,36 @@
     $('[data-toggle="tooltip"]').tooltip();
 
     //Add New Person in Edit Crew modal
-    $(document).on("click", ".add-person", function () {
-        var nextIndex = $("#people-table tbody tr").length;
+    $(document).on('click', '.add-person', function () {
+        var nextIndex = $('#people-table tbody tr').length;
 
         $.ajax({
-            url: "../../Movie/AddPerson",
+            url: '../../Movie/AddPerson',
             data: { index: nextIndex },
-            type: "GET"
+            type: 'GET'
         }).done(function (response) {
-            $("#people-table tbody").append(response);
+            $('#people-table tbody').append(response);
 
             //Refresh bootstrap-select so that it sets up the JS for new dropdowns
-            $(".selectpicker").selectpicker();
+            $('.selectpicker').selectpicker();
         });
     });
 
     //Delete Person in Edit Crew modal
-    $("#people-table").on("click", ".delete-person", function () {
-        $(this).closest("td").find("input").val("true");
-        $(this).closest("tr").hide();
+    $('#people-table').on('click', '.delete-person', function () {
+        $(this).closest('td').find('input').val('true');
+        $(this).closest('tr').hide();
     });
+
+    //Update image modals
+    $('.image-modal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+
+        var title = button.data('title'); 
+        var imageSource = button.data('img-src'); 
+
+        var modal = $(this);
+        modal.find('.modal-title').text(title);
+        modal.find('img').attr('src', imageSource);
+    })
 });

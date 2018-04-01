571d7bd5464e0e0953523d988a4cf8a46344859b
Levi <levihassel@gmail.com>
2018-04-01 12:15:07 -0500

    Made progress on seeding

--- DotNetFlicks/DotNetFlicks.Accessors/Accessors/MovieAccessor.cs
+++ DotNetFlicks/DotNetFlicks.Accessors/Accessors/MovieAccessor.cs
@@ -6,11 +6,125 @@ using DotNetFlicks.Accessors.Models.DTO;
 using DotNetFlicks.Accessors.Models.EF;
 using DotNetFlicks.Accessors.Models.EF.Base;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace DotNetFlicks.Accessors.Accessors
 {
+    internal class BelongsToCollection
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string poster_path { get; set; }
+        public string backdrop_path { get; set; }
+    }
+
+    internal class JsonGenre
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    internal class ProductionCompany
+    {
+        public int id { get; set; }
+        public string logo_path { get; set; }
+        public string name { get; set; }
+        public string origin_country { get; set; }
+    }
+
+    internal class ProductionCountry
+    {
+        public string iso_3166_1 { get; set; }
+        public string name { get; set; }
+    }
+
+    internal class SpokenLanguage
+    {
+        public string iso_639_1 { get; set; }
+        public string name { get; set; }
+    }
+
+    internal class Result
+    {
+        public string id { get; set; }
+        public string iso_639_1 { get; set; }
+        public string iso_3166_1 { get; set; }
+        public string key { get; set; }
+        public string name { get; set; }
+        public string site { get; set; }
+        public int size { get; set; }
+        public string type { get; set; }
+    }
+
+    internal class Videos
+    {
+        public List<Result> results { get; set; }
+    }
+
+    internal class JsonCast
+    {
+        public int cast_id { get; set; }
+        public string character { get; set; }
+        public string credit_id { get; set; }
+        public int gender { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int order { get; set; }
+        public string profile_path { get; set; }
+    }
+
+    internal class JsonCrew
+    {
+        public string credit_id { get; set; }
+        public string department { get; set; }
+        public int gender { get; set; }
+        public int id { get; set; }
+        public string job { get; set; }
+        public string name { get; set; }
+        public string profile_path { get; set; }
+    }
+
+    internal class Credits
+    {
+        public List<JsonCast> cast { get; set; }
+        public List<JsonCrew> crew { get; set; }
+    }
+
+    internal class RootObject
+    {
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public BelongsToCollection belongs_to_collection { get; set; }
+        public int budget { get; set; }
+        public List<JsonGenre> genres { get; set; }
+        public string homepage { get; set; }
+        public int id { get; set; }
+        public string imdb_id { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public double popularity { get; set; }
+        public string poster_path { get; set; }
+        public List<ProductionCompany> production_companies { get; set; }
+        public List<ProductionCountry> production_countries { get; set; }
+        public string release_date { get; set; }
+        public int revenue { get; set; }
+        public int runtime { get; set; }
+        public List<SpokenLanguage> spoken_languages { get; set; }
+        public string status { get; set; }
+        public string tagline { get; set; }
+        public string title { get; set; }
+        public bool video { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
+        public Videos videos { get; set; }
+        public Credits credits { get; set; }
+    }
+
+
     public class MovieAccessor : EntityAccessor<Entity>, IMovieAccessor
     {
         public MovieAccessor(DotNetFlicksContext db) : base(db)
@@ -19,6 +133,11 @@ namespace DotNetFlicks.Accessors.Accessors
 
         public MovieDTO Get(int id)
         {
+            var genres = JsonConvert.DeserializeObject<List<Genre>>(File.ReadAllText(@"Database" + Path.DirectorySeparatorChar + "SeedData" + Path.DirectorySeparatorChar + "genres.json"));
+
+            var jsonMovies = JsonConvert.DeserializeObject<List<RootObject>>(File.ReadAllText(@"Database" + Path.DirectorySeparatorChar + "SeedData" + Path.DirectorySeparatorChar + "movies.json"));
+            var movies = Mapper.Map<List<Movie>>(jsonMovies);
+
             var entity = _db.Movies
                 .Include(x => x.Cast).ThenInclude(x => x.Person)
                 .Include(x => x.Crew).ThenInclude(x => x.Person)
--- DotNetFlicks/DotNetFlicks.Accessors/Config/AccessorMapper.cs
+++ DotNetFlicks/DotNetFlicks.Accessors/Config/AccessorMapper.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using DotNetFlicks.Accessors.Accessors;
 using DotNetFlicks.Accessors.Models.DTO;
 using DotNetFlicks.Accessors.Models.EF;
+using System;
+using System.Linq;
 
 namespace DotNetFlicks.Accessors.Config
 {
@@ -16,6 +19,34 @@ namespace DotNetFlicks.Accessors.Config
             CreateMap<MovieGenre, MovieGenreDTO>().ReverseMap();
             CreateMap<Person, PersonDTO>().ReverseMap();
             CreateMap<UserMovie, UserMovieDTO>().ReverseMap();
+
+            CreateMap<RootObject, Movie>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.title))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.overview))
+                .ForMember(dest => dest.ReleaseDate, opt => opt.MapFrom(src => Convert.ToDateTime(src.release_date))) //see if this works
+                .ForMember(dest => dest.Runtime, opt => opt.MapFrom(src => TimeSpan.FromMinutes(src.runtime))) //see if this works
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => "https://image.tmdb.org/t/p/w600_and_h900_bestv2/" + src.poster_path)) //see if this works
+                .ForMember(dest => dest.TrailerUrl, opt => opt.MapFrom(src => "https://www.youtube.com/watch?v=" + src.videos.results.First().key)) //see if this works
+                .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.genres))
+                .ForMember(dest => dest.Cast, opt => opt.MapFrom(src => src.credits.cast))
+                .ForMember(dest => dest.Crew, opt => opt.MapFrom(src => src.credits.crew));
+
+            CreateMap<JsonGenre, MovieGenre>()
+                .ForMember(dest => dest.MovieId, opt => opt.MapFrom(src => src.id)) //leave blank, populate later?
+                .ForMember(dest => dest.GenreId, opt => opt.MapFrom(src => src.id));
+
+            CreateMap<JsonCast, CastMember>()
+                .ForMember(dest => dest.PersonId, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.MovieId, opt => opt.MapFrom(src => src.name)) //leave blank, populate later?
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.character))
+                .ForMember(dest => dest.ScreenTime, opt => opt.MapFrom(src => src.order)); //need DB change to get this working
+
+            CreateMap<JsonCrew, CrewMember>()
+                .ForMember(dest => dest.PersonId, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.MovieId, opt => opt.MapFrom(src => src.name)) //leave blank, populate later?
+                .ForMember(dest => dest.Position, opt => opt.MapFrom(src => src.job))
+                .ForMember(dest => dest.DepartmentId, opt => opt.MapFrom(src => src.department)); //find a way to get Id from name
         }
 
         public override string ProfileName

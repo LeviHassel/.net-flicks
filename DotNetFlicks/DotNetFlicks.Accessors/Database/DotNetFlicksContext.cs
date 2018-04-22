6add045852e5b07b554dde96131c8e43263b6ed1
Levi Hassel <levihassel@gmail.com>
2018-04-22 16:20:48 -0500

    Various View Movie Improvements

--- DotNetFlicks/DotNetFlicks.Accessors/Database/DotNetFlicksContext.cs
+++ DotNetFlicks/DotNetFlicks.Accessors/Database/DotNetFlicksContext.cs
@@ -19,9 +19,9 @@ namespace DotNetFlicks.Accessors.Database
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed. Add your customizations after calling base.OnModelCreating(builder);
-
+            base.OnModelCreating(builder);
+            
             //Disable cascade deletions
             foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
             {

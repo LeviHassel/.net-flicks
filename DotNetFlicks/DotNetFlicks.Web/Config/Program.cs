69b5ca4feb63f948d15b7234dc7786389397880c
Levi Hassel <levihassel@gmail.com>
2018-06-02 19:14:05 -0500

    Fixed admin seeding

--- DotNetFlicks/DotNetFlicks.Web/Config/Program.cs
+++ DotNetFlicks/DotNetFlicks.Web/Config/Program.cs
@@ -1,13 +1,5 @@
-﻿using DotNetFlicks.Accessors.Database;
-using DotNetFlicks.Accessors.Identity;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Linq;
 
 namespace DotNetFlicks.Web.Config
 {
@@ -17,39 +9,8 @@ namespace DotNetFlicks.Web.Config
         {
             var host = CreateWebHostBuilder(args).Build();
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<DotNetFlicksContext>();
-
-                    //Create database if it doesn't exist and apply any pending migrations (this will also seed data)
-                    context.Database.Migrate();
-
-                    //Seed admin user
-                    if (!context.Users.Any())
-                    {
-                        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-
-                        var admin = new ApplicationUser
-                        {
-                            UserName = "admin@dotnetflicks.com",
-                            Email = "admin@dotnetflicks.com",
-                            EmailConfirmed = true
-                        };
-
-                        userManager.CreateAsync(admin, "p@ssWORD471");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-
+            DbInitializer.Run(host);
+            
             host.Run();
         }
 
